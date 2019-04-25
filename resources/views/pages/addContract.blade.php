7d0e82cf66cdca51b4767e654985157f8df6e396
Samai DUCH <duchsamai77@gmail.com>
2019-04-25 19:52:19 +0700

    edit model contract

--- resources/views/pages/addContract.blade.php
+++ resources/views/pages/addContract.blade.php
@@ -118,7 +118,7 @@
                                                                                 <div class="input-group col-md-12">
                                                                                         <input class="form-control py-2" type="search" value="search" id="example-search-input">
                                                                                         <span class="input-group-append">
-                                                                                            <button class="btn btn-outline-secondary bg-info text-white" data-toggle="modal" data-target="#selectAddContract" type="button" style="margin-top:0%;">
+                                                                                            <button class="btn btn-outline-secondary bg-info text-white" data-toggle="modal" data-target="#select" type="button" style="margin-top:0%;">
                                                                                                Select
                                                                                             </button>
                                                                                         </span>
@@ -222,12 +222,11 @@
                 </div>
             </div>
 
-            <script type="text/javascript">
-//  modelad edit a new contract
+            
+{{-- //  modelad edit a new contract --}}
 
 
 
- {{-- modelad edit a new contract --}}
 
  <div class="modal fade bd-example-modal-lg" id="editContract" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-lg">
@@ -382,7 +381,7 @@
                                       <div class="card-body">
                                             <div class="modal-body">
                                                    
-                                                    <table id="myTable4" class="table table-striped table-bordered" style="width:100%">
+                                                    <table id="myTable5" class="table table-striped table-bordered" style="width:100%">
                                                             <thead class="bg-dark text-white">
                                                                 <tr>
                                                                     <th class="text-center ">ID</th>
@@ -500,6 +499,137 @@
                         </div>
                     </div>
             
+{{-- select add contract --}}
+
+{{-- model select contract  --}}
+
+<div class="modal fade bd-example" id="select"  tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+        <div class="modal-dialog ">
+            <div class="modal-content">
+                    <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Create new contract type</h5>
+                          </div>
+                          <div class="card-body">
+                                <div class="modal-body">
+                                       
+                                        <table id="myTable4" class="table table-striped table-bordered" style="width:100%">
+                                                <thead class="bg-dark text-white">
+                                                    <tr>
+                                                        <th class="text-center ">ID</th>
+                                                        <th> Contract type</th>
+                                                         
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                  
+                                                    <tr >
+                                                        <td class="text-center">
+                                                            <a href="#"  data-toggle="modal" data-target="#deleteContractType"><i class="material-icons text-danger">delete</i></a>
+                                                            <a href="#"  data-toggle="modal" data-target="#updateContractType"><i class="material-icons text-success">edit</i></a>
+                                                           1
+                                                        </td>
+                                                        <td>Car</td>
+                                                    </tr>
+                                                    <tr >
+                                                        <td class="text-center">
+                                                            <a href="#"  data-toggle="modal" data-target="#deleteContractType"><i class="material-icons text-danger">delete</i></a>
+                                                            <a href="#"  data-toggle="modal" data-target="#updateContractType"><i class="material-icons text-success">edit</i></a>
+                                                           2
+                                                        </td>
+                                                        <td>Moto</td>
+                                                       
+                                                    </tr>
+                                                    <tr >
+                                                        <td class="text-center">
+                                                            <a href="#"  data-toggle="modal" data-target="#deleteContractType"><i class="material-icons text-danger">delete</i></a>
+                                                            <a href="#"  data-toggle="modal" data-target="#updateContractType"><i class="material-icons text-success">edit</i></a>
+                                                           3
+                                                        </td>
+                                                        <td>House</td>
+                                                       
+                                                    </tr>
+                                                </tbody>
+                                               
+                                            </table>
+                            
+                                            
+                                        
+                              
+                                    
+                                    
+                              {{-- model update --}}
+                            
+                              <div class="modal fade" id="updateContractType" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                    <div class="modal-dialog" role="document">
+                                      <div class="modal-content">
+                                        <div class="modal-header">
+                                          <h5 class="modal-title" id="exampleModalLabel">Edit contract type</h5>
+                                          
+                                        </div>
+                                        <div class="modal-body">
+                                          <form action="#" method="POST" >
+                                              <div class="row">
+                                                  <div class="col-4"><p>Type of contract</p></div>
+                                                  <div class="col-7">
+                                                      <div class="form-group">
+                                                      
+                                                          <input type="text" name="contracttype" id="" class="form-control">
+                                                      </div>
+                                                  </div>
+                                                </div>
+                                          </form>
+                                        </div>
+                                        <div class="modal-footer">
+
+                                          <button type="button" class="btn btn-info" data-dismiss="modal">OK</button>
+                                         
+
+                                          <button type="button" class="btn btn-danger" data-dismiss="modal">Cancel</button>
+                                        </div>
+                                      </div>
+                                    </div>
+                                  </div>
+                            
+                                  {{-- model delete --}}
+                              <div class="modal fade" id="deleteContractType" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                    <div class="modal-dialog" role="document">
+                                      <div class="modal-content">
+                                        <div class="modal-header">
+                                          <h5 class="modal-title" id="exampleModalLabel">Confirmation</h5>
+                                          
+                                        </div>
+                                        <div class="modal-body">
+                                          <form action="#" method="POST" >
+                                             <p class="text-danger">Are you sure that you want to delete this contract type?</p>
+                                          </form>
+                                        </div>
+                                        <div class="modal-footer">
+
+                                          <button type="button" class="btn btn-info" data-dismiss="modal">OK</button>
+                                          <button type="button" class="btn btn-danger" data-dismiss="modal">No</button>
+
+
+                                        </div>
+                                      </div>
+                                    </div>
+                                  </div>
+                            
+                                        
+                                </div>
+                             </div>
+                             <div class="modal-footer">
+
+                                <button type="button" class="btn btn-info" data-dismiss="modal">OK</button>
+
+                             
+
+                                <button type="button" class="btn btn-danger" data-dismiss="modal">Cancel</button>
+                              </div>
+                        </div>
+                 </div>
+            </div>
+        </div>
+
 
             <script>
                 $(function () {
