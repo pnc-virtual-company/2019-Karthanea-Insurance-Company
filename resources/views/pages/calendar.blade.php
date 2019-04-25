be768381cc64bed9e7f590e5add1c86e9173416d
Bunsin NANH <bunsinnanh22@gmail.com>
2019-04-25 20:19:16 +0700

    [MenuEditing] Edit menu and Calendar refs MenuEditing

--- resources/views/pages/calendar.blade.php
+++ resources/views/pages/calendar.blade.php
@@ -1,7 +1,3 @@
-<!-- pei chart link -->
-<script src="https://cdnjs.com/libraries/Chart.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
-<script src="{{asset('js/chart.js')}}"></script>
 @extends('layout.dashboard')
 @section('content')
     <div class="container mt-4">
