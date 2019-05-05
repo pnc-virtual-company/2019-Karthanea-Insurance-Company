af3321206f964239a0fcac8ff8bb049d7be18582
Samai DUCH <duchsamai77@gmail.com>
2019-05-05 19:53:39 +0700

    create update active client

--- resources/views/pages/AchiveClient.blade.php
+++ resources/views/pages/AchiveClient.blade.php
@@ -1,11 +1,7 @@
-
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-
 @extends('layout.dashboard')
 @section('content')
     <div class="container mt-4">
-        <h1>Information Active Client</h1>
+        <h1>Information Active Client </h1>
         <div class="card">
             <div class="card-body">
                 <table id="myTable" class="table table-striped table-bordered table-hover">
@@ -19,17 +15,16 @@
                         </tr>
                     </thead>
                     <tbody>
-                        @foreach ($client as $item)
+                        @foreach ($client as $value=>$item)
+                        
                         <tr>
                             <td>
-                            <a href="{{route('achiveclient.update',$item->id)}}" data-toggle="modal" data-target="#editModalActivClient" data-id="{{$item->id_client}}"
-                                data-firstname="{{$item->firstname}}" data-lastname="{{$item->lastname}}"
-                                data-address="{{$item->address}}" data-phonenumber="{{$item->phonenumber}}"
-                                data-email="{{$item->email}}"> <i class="material-icons text-success">edit</i></a>
+                                <a href="{{route('achiveclient.update',$item->id)}}" data-toggle="modal"  data-target="#editClient" data-id="{{$item->id}}" data-firstname="{{$item->firstname}}" data-lastname="{{$item->lastname}}" data-address="{{$item->address}}" data-phonenumber="{{$item->phonenumber}}" data-email="{{$item->email}}"><i class="material-icons text-success">edit</i></a>
+                               
                                 <input type="checkbox" name="disable" id="disable">
                                 {{$item->id}}
                             </td>
-                            <td>{{$item->firstname}} {{$item->lastname}}</td>
+                            <td>{{$item->firstname}} {{$item->lastname}} </td>
                             <td>{{$item->address}}</td>
                             <td>{{$item->phonenumber}}</td>
                             <td>{{$item->email}}</td>
@@ -38,11 +33,11 @@
                     </tbody>
                 </table>
                 <div>
-                    <button type="button" class="btn bg-primary" data-toggle="modal" data-target="#createClient"><i class="material-icons">control_point</i> Create Client</button>
+                    <button type="button" class="btn text-white bg-primary" data-toggle="modal" data-target="#createClient"><i class="material-icons">control_point</i> Create Client</button>
                 </div>
             </div>
         </div>
-      </div>
+    </div>
           <!-- Modal add -->
           <div class="modal fade" id="createClient" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div class="modal-dialog" role="document">
@@ -53,16 +48,17 @@
                     <span aria-hidden="true">&times;</span>
                   </button>
                 </div>
-            <form id="form" action="{{action('clientAchiveController@store')}}" method="POST">
+                <form method="POST" action="{{action('clientAchiveController@store')}}">
                 @csrf
                 <div class="modal-body">
+
                         <div class="form-group">
                             <div class="row">
                                 <div class="col-2">
                                     <label for="name">Firstname</label>
                                 </div>
                                 <div class="col-10">
-                                    <input type="text" name="firstname" class="form-control">
+                                    <input type="text" name="firstname"  class="form-control">
                                 </div>
                             </div>
                         </div>
@@ -72,7 +68,7 @@
                                     <label for="name">Lastname</label>
                                 </div>
                                 <div class="col-10">
-                                    <input type="text" name="lastname" class="form-control">
+                                    <input type="text" name="lastname"  class="form-control">
                                 </div>
                             </div>
                         </div>
@@ -82,9 +78,7 @@
                                     <label for="address">Address</label>
                                 </div>
                                 <div class="col-10">
-
-                                    <input type="text" name="address" class="form-control">
-
+                                    <input type="text" name="address"  class="form-control">
                                 </div>
                             </div>
                         </div>
@@ -95,7 +89,6 @@
                                 </div>
                                 <div class="col-10">
                                     <input type="number" name="phonenumber"  class="form-control">
-
                                 </div>
                             </div>
                         </div>
@@ -111,17 +104,15 @@
                         </div>
                 </div>
                 <div class="modal-footer">
-                    <button type="submit" class="btn bg-primary text-white">OK</button>
-
-                    <button type="button" class="btn btn-danger" data-dismiss="modal">Cancel</button>
+                    <button type="submit" class="btn btn-info">OK</button>
+                  <button type="button" class="btn btn-danger" data-dismiss="modal">Cancel</button>
                 </div>
             </form>
               </div>
             </div>
           </div>
-
           <!-- Modal Edit -->
-          <div class="modal fade" id="editModalActivClient" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal fade" id="editClient" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div class="modal-dialog" role="document">
               <div class="modal-content">
                 <div class="modal-header">
@@ -129,7 +120,7 @@
                 
                 </div>
                 
-                <form action="" method="POST" id="editActivClient">
+                <form action="" method="POST" id="editClientList">
                     @csrf
                     @method('PATCH')
                    
@@ -198,7 +189,7 @@
           
           <script src="{{asset('js/app.js')}}"></script>
 <script>
-        $('#editModalActivClient').on('show.bs.modal',function (event){
+        $('#editClient').on('show.bs.modal',function (event){
               var button = $(event.relatedTarget)
               var firstname = button.data('firstname')
               console.log(firstname)
@@ -221,10 +212,10 @@
               modal.find('#phonenumber').attr('value',phonenumber)
               modal.find('#email').attr('value',email)
   
-              var url ="{{url('/achiveclient')}}/"+ id;
-              $('#editActivClient').attr('action',url);   
+              var url ="{{url('/client')}}/"+ id;
+              $('#editClientList').attr('action',url);   
               });
           
 
         </script>
-          @endsection
+@endsection
