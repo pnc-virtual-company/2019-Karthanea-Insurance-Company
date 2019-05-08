5bc64d1867bbe1ed3eb6ac0d66b5ea569a352193
Bunsin NANH <bunsinnanh22@gmail.com>
2019-05-08 09:10:22 +0700

    edit

--- resources/views/pages/AchiveClient.blade.php
+++ resources/views/pages/AchiveClient.blade.php
@@ -190,8 +190,8 @@
 
 
 </body> 
-{{-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script> --}}
-<script src="{{asset('js/app.js')}}"></script>
+{{-- <script src="{{asset('js/app.js')}}"></script> --}}
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 <script type="text/javascript">
     $('#editClient').on('show.bs.modal',function(event){
     var button = $(event.relatedTarget)
