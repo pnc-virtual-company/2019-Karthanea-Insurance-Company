b9d951cd2c6a9e668efd6581404b4f804b6c9c31
Bunsin NANH <bunsinnanh22@gmail.com>
2019-05-14 13:46:48 +0700

    Payment Crud feature

---
+++ resources/views/pages/clientContract.blade.php
@@ -0,0 +1,7 @@
+<div class="container">
+    
+</div>
+<script src="{{asset('js/app.js')}}"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
